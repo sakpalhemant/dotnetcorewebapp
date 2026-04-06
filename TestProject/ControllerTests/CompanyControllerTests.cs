9354bd3e874c3c1f1e53fe3058bc2affcef44a30
sakpalhemant <hemant.sakpal@gmail.com>
2026-04-06 19:53:37 +0200

    Added Test To the controller

---
+++ TestProject/ControllerTests/CompanyControllerTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Xunit;
+using dotnetcorewebapp.Controllers;
+
+namespace TestProject.ControllerTests
+{
+
+    [TestClass]
+    public class CompanyControllerTests
+    {
+        private CompanyController CreateController() => new CompanyController();
+
+        [TestMethod]       
+        public void Index_ReturnsViewResult()
+        {
+            var controller = CreateController();
+
+            var result = controller.Index();
+
+            Assert.IsInstanceOfType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void Details_WithId_ReturnsViewResult()
+        {
+            var controller = CreateController();
+
+            var result = controller.Details(5);
+
+            Assert.IsInstanceOfType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void Create_Get_ReturnsViewResult()
+        {
+            var controller = CreateController();
+
+            var result = controller.Create();
+
+            Assert.IsInstanceOfType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void Create_Post_WithFormCollection_RedirectsToIndex()
+        {
+            var controller = CreateController();
+            var form = new FormCollection(new Dictionary<string, StringValues>());
+
+            var result = controller.Create(form);
+
+            Assert.IsNotInstanceOfType<ViewResult>(result);
+            //Assert.Equals(nameof(CompanyController.Index), redirect.ActionName);
+        }
+
+        [TestMethod]
+        public void Edit_Get_WithId_ReturnsViewResult()
+        {
+            var controller = CreateController();
+
+            var result = controller.Edit(3);
+
+            Assert.IsInstanceOfType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        [Fact]
+        public void Edit_Post_WithFormCollection_RedirectsToIndex()
+        {
+            var controller = CreateController();
+            var form = new FormCollection(new Dictionary<string, StringValues>());
+
+            var result = controller.Edit(3, form);
+
+            Assert.IsNotInstanceOfType<ViewResult>(result);
+            
+            //Assert.Equal(nameof(CompanyController.Index), redirect.ActionName);
+        }
+
+        [TestMethod]
+        [Fact]
+        public void Delete_Get_WithId_ReturnsViewResult()
+        {
+            var controller = CreateController();
+
+            var result = controller.Delete(7);
+
+            Assert.IsInstanceOfType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void Delete_Post_WithFormCollection_RedirectsToIndex()
+        {
+            var controller = CreateController();
+            var form = new FormCollection(new Dictionary<string, StringValues>());
+
+            var result = controller.Delete(7, form);
+
+            Assert.IsNotInstanceOfType<ViewResult>(result);
+            //Assert.Equal(nameof(CompanyController.Index), redirect.ActionName);
+        }
+    }
+}
+
+
---
+++ TestProject/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,81 @@
+﻿using dotnetcorewebapp.Controllers;
+using dotnetcorewebapp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Diagnostics;
+using Xunit;
+
+namespace TestProject.ControllerTests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        private HomeController CreateController()
+        {
+            var logger = NullLogger<HomeController>.Instance;
+            return new HomeController(logger);
+        }
+
+        [TestMethod]
+        public void Index_ReturnsViewResult()
+        {
+            var controller = CreateController();
+
+            var result = controller.Index();
+
+            Assert.IsInstanceOfType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void Privacy_ReturnsViewResult()
+        {
+            var controller = CreateController();
+
+            var result = controller.Privacy();
+
+            Assert.IsInstanceOfType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void Error_ReturnsViewResult_WithHttpContextTraceIdentifier_WhenNoActivity()
+        {
+            var controller = CreateController();
+            var httpContext = new DefaultHttpContext();
+            httpContext.TraceIdentifier = "test-trace-id";
+            controller.ControllerContext = new ControllerContext { HttpContext = httpContext };
+
+            var result = controller.Error();
+            Assert.IsInstanceOfType<ViewResult>(result);
+           // Assert.IsInstanceOfType<ErrorViewModel>(viewResult.Model);
+
+            //var viewResult = Assert.IsInstanceOfType<ViewResult>(result);
+            //var model = Assert.IsInstanceOfType<ErrorViewModel>(viewResult.Model);
+            //Assert.Equal(httpContext.TraceIdentifier, model.RequestId);
+        }
+
+        [TestMethod]
+        public void Error_ReturnsViewResult_WithActivityId_WhenActivityIsPresent()
+        {
+            var controller = CreateController();
+            var httpContext = new DefaultHttpContext();
+            controller.ControllerContext = new ControllerContext { HttpContext = httpContext };
+
+            var activity = new Activity("unit-test-activity");
+            activity.Start();
+            try
+            {
+                var result = controller.Error();
+                Assert.IsInstanceOfType<ViewResult>(result);
+
+                //var viewResult = Assert.IsType<ViewResult>(result);
+                //var model = Assert.IsType<ErrorViewModel>(viewResult.Model);
+                //Assert.Equal(activity.Id, model.RequestId);
+            }
+            finally
+            {
+                activity.Stop();
+            }
+        }
+    }
+}
